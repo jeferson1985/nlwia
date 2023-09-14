01b94e49077d0ac7e730fe6e8e2a1e71e93efe42
Jeferson <refej@hotmail.com>
2023-09-14 00:01:49 -0300

    Segundo dia

---
+++ server/download.js
@@ -0,0 +1,21 @@
+import ytdl from "ytdl-core"
+import fs from "fs"
+
+export const download = (videoId) => {
+  const videoUrl = "https://www.youtube.com/shorts/" + videoId
+  console.log("Realizando o download do video:", videoId)
+  ytdl(videoUrl, { quality: "lowestaudio", filter: "audioonly" })
+    .on("info", (info) => {
+      const seconds = info.formats[0].approxDurationMs / 1000
+      if (seconds > 60) {
+        throw new Error("Duração do video é maior que 60 segundos")
+      }
+    })
+    .on("end", () => {
+      console.log("Download do video finalizado")
+    })
+    .on("error", (error) => {
+      console.log("Não foi possivel realizar o download do video", error)
+    })
+    .pipe(fs.createWriteStream("./tmp/audio.mp4"))
+}
